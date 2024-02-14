522abf50c8945a94ead851f34a4831613da0fdc0
abdullaharshad143 <abdullaharshadrasheed@gmail.com>
2024-02-14 22:30:28 +0500

    added validation

--- src/screens/auth/AddressInfoScreen.tsx
+++ src/screens/auth/AddressInfoScreen.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useState } from "react";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { RootStackParamList } from "../../core/types";
-import { View, Text, TextInput, SafeAreaView, StyleSheet } from "react-native";
+import { FormErrors, RootStackParamList } from "../../core/types";
+import { View, Text, TextInput, SafeAreaView, StyleSheet, Alert, Keyboard } from "react-native";
 import { Colors } from "../../theme/color";
 import Header from "../../components/Header";
 import { horizontalScale, moderateScale, verticalScale } from "../../utils/responsive";
@@ -11,9 +11,34 @@ import Fonts from "../../theme/typographic";
 const AddressInfoScreen = ({
     navigation,
 }: NativeStackScreenProps<RootStackParamList>) => {
-    const handlePress = useCallback(() => {
+    const [postalCode, setPostalCode] = useState('');
+    const [sector, setSector] = useState('');
+    const [streetNo, setStreetNo] = useState('');
+    const [houseNo, setHouseNo] = useState('');
+    const [phoneNo, setPhoneNo] = useState('');
+    const [note, setNote] = useState('');
+    const [errors, setErrors] = useState<FormErrors>({})
 
-    }, [])
+    const validateForm = () => {
+        let validationErrors : FormErrors = {}
+        if(!postalCode)
+        validationErrors.postalCode = '*Postal Code is required'
+        if(!sector)
+        validationErrors.sector = '*Sector is required'
+        if(!streetNo)
+        validationErrors.streetNo = '*Street No is required'
+        if(!houseNo)
+        validationErrors.houseNo = '*House No is required'
+        if(!phoneNo)
+        validationErrors.phoneNumber = '*Phone No is required'
+        setErrors(validationErrors)
+        return Object.keys(validationErrors).length === 0
+        }
+    const handlePress = useCallback(() => {
+        const isValid = validateForm()
+        if (isValid)
+        Alert.alert("Good")
+    }, [postalCode, sector, streetNo, houseNo, phoneNo])
     return (
         <SafeAreaView style={styles.mainContainer}>
             <View >
@@ -30,37 +55,74 @@ const AddressInfoScreen = ({
                     >
                     </TextInput>
                 </View>
-                <View style={styles.inputContainer}>
+                <View style={styles.flexStyle}>
+                <View style={[styles.inputContainer, styles.smallInputsContainer]}>
                     <TextInput
                         style={styles.inputStyle}
                         placeholder="Postal Code"
                         keyboardType="number-pad"
+                        value={postalCode}
+                        onChangeText={setPostalCode}
                     >
                     </TextInput>
                 </View>
-                <View style={styles.inputContainer}>
+                <View style={[styles.inputContainer, styles.smallInputsContainer]}>
                     <TextInput
                         style={styles.inputStyle}
                         placeholder="Sector"
+                        value={sector}
+                        onChangeText={setSector}
                     >
                     </TextInput>
                 </View>
-                <View style={styles.inputContainer}>
+                </View>
+                {errors.postalCode && <Text style={styles.errorText}>{errors.postalCode}</Text>}
+                {errors.sector && <Text style={styles.errorText}>{errors.sector}</Text>}
+                <View style={styles.flexStyle}>
+                <View style={[styles.inputContainer, styles.smallInputsContainer]}>
                     <TextInput
                         style={styles.inputStyle}
                         placeholder="Street No"
+                        value={streetNo}
+                        onChangeText={setStreetNo}
                     >
                     </TextInput>
                 </View>
-                <View style={styles.inputContainer}>
+                <View style={[styles.inputContainer, styles.smallInputsContainer]}>
                     <TextInput
                         style={styles.inputStyle}
                         placeholder="House No"
+                        value={houseNo}
+                        onChangeText={setHouseNo}
+                    >
+                    </TextInput>
+                </View>
+                </View>
+                {errors.streetNo && <Text style={styles.errorText}>{errors.streetNo}</Text>}
+                {errors.houseNo && <Text style={styles.errorText}>{errors.houseNo}</Text>}
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        style={styles.inputStyle}
+                        placeholder="Phone No"
+                        value={phoneNo}
+                        onChangeText={setPhoneNo}
+                    >
+                    </TextInput>
+                </View>
+                {errors.phoneNumber && <Text style={styles.errorText}>{errors.phoneNumber}</Text>}
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        style={styles.inputStyle}
+                        placeholder="Delivery Note (optional)"
+                        value={note}
+                        onChangeText={setNote}
+                        multiline={true}
+                        numberOfLines={3}
+                        // onSubmitEditing={()=>Keyboard.dismiss}
                     >
                     </TextInput>
                 </View>
                 <Button title="Next" onPress={handlePress}>
-
                 </Button>
             </View>
         </SafeAreaView>
@@ -97,6 +159,18 @@ const styles = StyleSheet.create({
         flex:1,
         fontFamily:Fonts.Family.SemiBold,
     },
+    flexStyle:{
+        flexDirection:'row'
+    },
+    smallInputsContainer:{
+        width: '34%', 
+        marginHorizontal:10
+    },
+    errorText:{
+        width:'73%',
+        color: Colors.error,
+        fontFamily:Fonts.Family.MediumItalic
+    }
 });
 
 export default AddressInfoScreen
