1e74c3c239dbefa27645494c7851186f4f040974
abdullaharshad143 <abdullaharshadrasheed@gmail.com>
2024-02-16 19:30:22 +0500

    added env variables

--- config/firebase.js
+++ config/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBKkiWUTNgJdHSi0p_VnFD8FQEAlSm0c-Q",
-  authDomain: "foodapp-26d02.firebaseapp.com",
-  projectId: "foodapp-26d02",
-  storageBucket: "foodapp-26d02.appspot.com",
-  messagingSenderId: "682770046989",
-  appId: "1:682770046989:web:faf7836212496c65673544",
-  measurementId: "G-GM9W6FEY2M"
+  apiKey: process.env.API_KEY ,
+  authDomain: process.env.AUTH_DOMAIN ,
+  projectId: process.env.PROJECT_ID ,
+  storageBucket: process.env.STORAGE_BUCKET ,
+  messagingSenderId: process.env.MESSAGIN_SENDER_ID ,
+  appId: process.env.APP_ID ,
+  measurementId: process.env.MEASUREMENT_ID 
 };
 
 // Initialize Firebase
