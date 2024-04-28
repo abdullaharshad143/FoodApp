1b7381cf122f6f17eb2b30cb2235e03344d4c15a
abdullaharshad143 <abdullaharshadrasheed@gmail.com>
2024-04-28 18:31:54 +0500

    feature: added tab bar for the orders

--- src/Orders/OrdersScreen.tsx
+++ src/Orders/OrdersScreen.tsx
@@ -1,11 +1,78 @@
-import { Text } from "react-native"
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { Text, View, StyleSheet } from "react-native";
+import { RootStackParamList } from "../core/types";
+import { TabBar, TabView } from "react-native-tab-view";
+import { useState } from "react";
+import Fonts from "../theme/typographic";
+import { verticalScale } from "../utils/responsive";
+import UpcomingOrders from "./UpcomingOrdersScreen";
+import PastOrders from "./PastOrdersScreen";
 
-const OrdersScreen = () => {
-    return(
-        <Text>
-            Orders Screeen
-        </Text>
-    )
+const OrdersScreen = ({
+    navigation,
+}: NativeStackScreenProps<RootStackParamList>) => {
+    const [index, setIndex] = useState(0);
+    // Correctly initialize the routes state with an array of route objects
+    const [routes] = useState([
+        { key: 'upcoming', title: 'Upcoming' },
+        { key: 'past', title: 'Past' },
+    ]);
+
+    const renderScene = ({ route }) => {
+        switch (route.key) {
+            case 'upcoming':
+                return <UpcomingOrders />
+            case 'past':
+                return <PastOrders />
+            default:
+                return null;
+        }
+    };
+
+    const renderTabBar = (props: any) => (
+        <TabBar
+            {...props}
+            indicatorStyle={styles.indicator}
+            style={styles.tabBar}
+            labelStyle={styles.label}
+            renderLabel={({ route, focused, color }) => (
+                <Text style={[styles.labelText, {color: focused ? 'black' : 'grey',}]}>
+                    {route.title}
+                </Text>
+            )}
+        />
+    );
+
+    return (
+        <View style={styles.container}>
+            <TabView
+                navigationState={{ index, routes }}
+                renderScene={renderScene}
+                onIndexChange={setIndex}
+                renderTabBar={renderTabBar}
+            />
+        </View>
+    );
 }
 
-export default OrdersScreen
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    tabBar: {
+        backgroundColor: '#fff', // Customize tab bar background
+        paddingTop: verticalScale(30)
+    },
+    indicator: {
+        backgroundColor: 'black', // Customize tab indicator color
+    },
+    label: {
+        color: 'black', // Customize tab label color
+    },
+    labelText:{
+        fontFamily: Fonts.Family.SemiBold,
+        fontSize: 20 
+    }
+});
+
+export default OrdersScreen;
