5e1274210b84924b813c9be3fd1da3b800185830
abdullaharshad143 <abdullaharshadrasheed@gmail.com>
2024-07-06 15:29:16 +0500

    Implemented the feature to update the order

--- src/Cart/CartScreen.tsx
+++ src/Cart/CartScreen.tsx
@@ -1,5 +1,5 @@
 import { View, StyleSheet, ScrollView, Text } from "react-native"
-import React, { useEffect } from "react"
+import React, { useCallback, useEffect, useState } from "react"
 import { verticalScale } from "../utils/responsive"
 import { IProduce, RootBottomParamList, RootStackParamList, } from "../core/types"
 import { NativeStackScreenProps } from "@react-navigation/native-stack"
@@ -11,6 +11,9 @@ import { useSelector, useDispatch } from "react-redux"
 import { RootState } from "../redux/store"
 import Fonts from "../theme/typographic"
 import { clearCart } from "../redux/cart/cartSlices"
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import { doc, getDoc, updateDoc } from "firebase/firestore"
+import { db } from "../../config/firebase"
 
 const CartScreen = ({
     navigation,
@@ -20,10 +23,60 @@ const CartScreen = ({
     }, [])
     const cartItems = useSelector((state: RootState) => state.produce.items);
     const status = useSelector((state: any) => state.user.status);
+    const totalPrice = useSelector((state: RootState) => state.produce.totalPrice);
+    const priceWithDelivery = totalPrice ? totalPrice + 150 : 150;
+    const oneTimePrice = priceWithDelivery + 200;
     const dispatch = useDispatch();
+    const [loading, setLoading] = useState(false)
     const clearTheCart = () => {
         dispatch(clearCart());
     }
+
+    const handleUpdateOrder = useCallback(async () => {
+        setLoading(true);
+        const userId = await AsyncStorage.getItem('userId');
+        
+        if (!userId) {
+            console.error("User ID not found");
+            setLoading(false);
+            return;
+        }
+        
+        try {
+            const userDocRef = doc(db, "users", userId);
+            const userDoc = await getDoc(userDocRef);
+            
+            if (!userDoc.exists()) {
+                console.error("User document not found");
+                setLoading(false);
+                return;
+            }
+            
+            const orderId = userDoc.data().orderId;
+            
+            if (!orderId) {
+                console.error("Order ID not found");
+                setLoading(false);
+                return;
+            }
+    
+            const orderData = {
+                orderedItems: cartItems,
+                totalPrice: status === "ACTIVE" ? priceWithDelivery : oneTimePrice,
+                orderUpdateDate: new Date().toISOString(),
+            };
+    
+            const collectionName = status === "ACTIVE" ? "subscription" : "oneTime";
+            const orderDocRef = doc(db, collectionName, orderId);
+            await updateDoc(orderDocRef, orderData);
+            alert("Order Updated!");
+        } catch (error) {
+            console.error("Error updating document: ", error);
+        } finally {
+            setLoading(false);
+        }
+    }, [cartItems, totalPrice, navigation, dispatch]);
+
     return (
         <>
             {cartItems.length > 0 ? (
@@ -39,8 +92,8 @@ const CartScreen = ({
                         <PaymentDetails />
                     </View>
                     <View style={styles.buttonContiner}>
-                        {status === "ACTIVE" || status === "SCHEDULED" ? (
-                            <SmallButton title="Update" onPress={() => navigation.navigate("SubscribeAndSaveScreen")} />
+                        {status === "ACTIVE" || "SCHEDULED" || "PAUSE" ? (
+                            <SmallButton title="Update" loading={loading} onPress={handleUpdateOrder} />
                         ) : (
                             <SmallButton title="Checkout" onPress={() => navigation.navigate("SubscribeAndSaveScreen")} />
                         )}
